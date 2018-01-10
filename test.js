34330efc238e64d78b5ad912f02dce40cb4245a0
Austin Kregel <austin@kbcomp.co>
2018-01-10 05:49:34 +0000

    Move test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-const app = express()
-
-const router = require('./index')(app);
-
-router.get('/', (req, res) => ('Hello World!'))
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
