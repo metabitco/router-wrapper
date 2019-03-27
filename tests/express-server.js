0690ccf1bf926c9f94eec07fa69f3dd1ab3df42c
Austin Kregel <austin@kbcomp.co>
2019-03-27 00:47:59 -0400

    Update the tests to include the more methods

--- tests/express-server.js
+++ tests/express-server.js
@@ -19,5 +19,33 @@ router.get('/middleware', (req, res) => ('Hello World!'), (req, res) => {
     return res.send('Middleware intercept');
 })
 
+router.middleware((req, res) => {
+    return res.send('Middleware intercept');
+}).get('/middleware-method', (req, res) => ('Hello World!'))
+
+class FakeController {
+    index() {
+        return 'Hello Api World!'
+    }
+
+    store() {
+        return 'Hello Api Store!'
+    }
+
+    update() {
+        return 'Hello Api Update!'
+    }
+
+    destroy() {
+        return 'Hello Api Destroy!'
+    }
+
+    show() {
+        return 'Hello Api Show!'
+    }
+}
+
+router.resource('api', new FakeController(), 'a');
+
 module.exports = app.listen(3000, () => console.log('Example app listening on port 3000!'))
 
--- tests/express-spec.js
+++ tests/express-spec.js
@@ -8,40 +8,77 @@ describe('loading express', function () {
     beforeEach(function () {
         server = require('./express-server');
     });
+
     afterEach(function () {
         server.close();
     });
+
     it('responds to /', function testSlash(done) {
         request(server)
             .get('/')
             .expect(200, done);
     });
+
     it('can receive a post request', function testPath(done) {
         request(server)
             .post('/post', {})
             .expect(200, done);
     });
+
     it('can receive a put request', function testPath(done) {
         request(server)
             .put('/put', {})
             .expect(200, done);
     });
+
     it('can receive a delete request', function testPath(done) {
         request(server)
             .delete('/delete', {})
             .expect(200, done);
     });
+
     it('can receive a patch request', function testPath(done) {
         request(server)
             .patch('/patch', {})
             .expect(200, done);
     });
-    it('has middleware intercept a desired route', function testPath(done) {
+
+    it('gets intercepted by middleware method for a desired route', (done) => {
         request(server)
-            .get('/middleware', {})
+            .get('/middleware-method', {})
             .end (function (err, res) {
-                 assert.equal('Middleware intercept', res.text);
-                 done();
-             });
+                assert.equal('Middleware intercept', res.text);
+                done();
+            });
+    });
+
+    it('responds to / on resource route', function testSlash(done) {
+        request(server)
+            .get('/api')
+            .expect(200, done);
+    });
+
+    it('can receive a post request on resource route', function testPath(done) {
+        request(server)
+            .post('/api', {})
+            .expect(200, done);
+    });
+
+    it('can receive a put request on resource route', function testPath(done) {
+        request(server)
+            .put('/api/a', {})
+            .expect(200, done);
+    });
+
+    it('can receive a delete request on resource route', function testPath(done) {
+        request(server)
+            .delete('/api/a', {})
+            .expect(200, done);
+    });
+
+    it('can receive a patch request on resource route', function testPath(done) {
+        request(server)
+            .patch('/api/a', {})
+            .expect(200, done);
     });
 });
